af0c79230b151a569cb2b9b0df68d0d0d97e1df7
David McCullough <dmccullough488@gmail.com>
2025-04-30 13:02:15 -0500

    created new fireB project & realtime DB and removed unneeded code

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js"
-const firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js"
 
+const firebaseConfig = {
+  databaseURL: "https://leads-tracker-app-d084d-default-rtdb.firebaseio.com"
 }
 
 const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
 
-let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-const tabBtn = document.getElementById("tab-btn")
-
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage
-  render(myLeads)
-}
-
-tabBtn.addEventListener("click", function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
-  })
-})
 
 function render(leads) {
   let listItems = ""
@@ -41,14 +28,9 @@ function render(leads) {
 }
 
 deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear()
-  myLeads = []
-  render(myLeads)
 })
 
 inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value)
+  console.log(inputEl.value)
   inputEl.value = ""
-  localStorage.setItem("myLeads", JSON.stringify(myLeads))
-  render(myLeads)
-})
+}) 
