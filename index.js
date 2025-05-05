e86404e4b39c9448620b2c06f6ec53b4703315c3
David McCullough <dmccullough488@gmail.com>
2025-05-05 11:51:03 -0500

    push a lead to firebase realtime db

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
+import {
+  getDatabase,
+  ref,
+  push
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
 
 const firebaseConfig = {
-  databaseURL: "https://leads-tracker-app-d084d-default-rtdb.firebaseio.com"
+  databaseURL: process.env.DATABASE_URL
 }
 
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
+const referenceInDB = ref(database, "leads")
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -28,9 +33,10 @@ function render(leads) {
 }
 
 deleteBtn.addEventListener("dblclick", function () {
+
 })
 
 inputBtn.addEventListener("click", function () {
-  console.log(inputEl.value)
+  push(referenceInDB, inputEl.value)
   inputEl.value = ""
-}) 
+})
